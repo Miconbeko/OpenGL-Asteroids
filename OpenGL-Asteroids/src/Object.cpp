28d70b91ecac1ed263d09de23da9c60e38d8f81d
Miconbeko <stepanov.m2004@gmail.com>
2023-01-22 21:25:08 +0200

    style: project clear

---
+++ OpenGL-Asteroids/src/Object.cpp
@@ -0,0 +1,94 @@
+#include "../inc/Object.h"
+
+namespace Asteroids
+{
+
+    Object::Object(float xCenter, float yCenter, float zCenter,
+        float xSize, float ySize, float zSize,
+        float* diffColor, float* ambiColor, float* specColor,
+        float xSpeed, float ySpeed, float polygones,
+        float rotateSpeed, float angle, float rotateVectorX, float rotateVectorY, float rotateVectorZ)
+        : polygones(polygones), rotateSpeed(rotateSpeed), angle(angle)
+    {
+        setCoords(xCenter, yCenter, zCenter);
+        setSize(xSize, ySize, zSize);
+        setColors(diffColor, ambiColor, specColor);
+        setSpeed(xSpeed, ySpeed);
+        setRotateVector(rotateVectorX, rotateVectorY, rotateVectorZ);
+    }
+
+    void Object::calcCoords()
+    {
+        this->setCoords(xCenter + getXSpeed(), yCenter, zCenter + getYSpeed());
+
+        setAngle(angle + rotateSpeed);
+    }
+
+    bool Object::isCollision(Object* other)
+    {
+        if (sqrt(pow(abs(xCenter - other->xCenter), 2) +
+            pow(abs(yCenter - other->yCenter), 2) +
+            pow(abs(zCenter - other->zCenter), 2)) <
+            (min(min(xSize, ySize), zSize) / 2 + min(min(other->xSize, other->ySize), other->zSize) / 2))
+            return true;
+        return false;
+    }
+
+    float Object::calcAngleMultiply(bool isForX)
+    {
+        int tempAngle = angle;
+
+        if (tempAngle < 0) tempAngle = 360 + tempAngle;
+        float answer = 1 - (((tempAngle + 90 * int(isForX)) % 180) * 0.0111111);
+
+        if (!isForX)
+        {
+            if (tempAngle >= 180 && tempAngle < 360)
+                return -answer;
+            else
+                return answer;
+        }
+        else
+        {
+            if (tempAngle >= 0 && tempAngle < 90 || tempAngle >= 270 && tempAngle < 360)
+                return -answer;
+            else
+                return answer;
+        }
+        return 0;
+    }
+
+    void Object::setCoords(float xCenter, float yCenter, float zCenter)
+    {
+        this->xCenter = xCenter;
+        this->yCenter = yCenter;
+        this->zCenter = zCenter;
+    }
+
+    void Object::setSize(float xSize, float ySize, float zSize)
+    {
+        this->xSize = xSize;
+        this->ySize = ySize;
+        this->zSize = zSize;
+    }
+
+    void Object::setColors(float* diffColor, float* ambiColor, float* specColor)
+    {
+        this->diffColor = diffColor;
+        this->ambiColor = ambiColor;
+        this->specColor = specColor;
+    }
+
+    void Object::setSpeed(float x, float y)
+    {
+        this->xSpeed = x;
+        this->ySpeed = y;
+    }
+
+    void Object::setRotateVector(float rotateVectorX, float rotateVectorY, float rotateVectorZ)
+    {
+        this->rotateVectorX = rotateVectorX;
+        this->rotateVectorY = rotateVectorY;
+        this->rotateVectorZ = rotateVectorZ;
+    }
+}
---
+++ OpenGL-Asteroids/src/utils.cpp
@@ -0,0 +1,54 @@
+#include "../inc/utils.h"
+
+namespace GraphUtils
+{
+    float diffWhite[] = { 1.0f, 1.0f, 1.0f };
+    float ambiWhite[] = { 0.8f, 0.8f, 0.8f };
+    float specWhite[] = { 1.0f, 1.0f, 1.0f };
+
+    float diffBlue[] = { 0.0f, 0.0f, 0.6f };
+    float ambiBlue[] = { 0.1f, 0.1f, 0.2f };
+    float specBlue[] = { 0.2f, 0.2f, 0.8f };
+
+    float diffGray[] = { 0.6f, 0.6f, 0.6f };
+    float ambiGray[] = { 0.2f, 0.2f, 0.2f };
+    float specGray[] = { 0.8f, 0.8f, 0.8f };
+
+    float diffRed[] = { 0.6f, 0.0f, 0.0f };
+    float ambiRed[] = { 0.2f, 0.1f, 0.1f };
+    float specRed[] = { 0.8f, 0.2f, 0.2f };
+
+    float diffYellow[] = { 0.9f, 0.9f, 0 };
+    float ambiYellow[] = { 0.2f, 0.2f, 0.1f };
+    float specYellow[] = { 1.0f, 1.0f, 0.2f };
+
+    float diffGreen[] = { 0, 0.5f, 0 };
+    float ambiGreen[] = { 0.1f, 0.2f, 0.1f };
+    float specGreen[] = { 0.2f, 0.7f, 0.2f };
+
+    float diffOrange[] = { 0.9f, 0.2f, 0 };
+    float ambiOrange[] = { 0.2f, 0.2f, 0.2f };
+    float specOrange[] = { 0.8f, 0.8f, 0.8f };
+
+    float diffLightBlue[] = { 0, 0.6f, 0.9f };
+    float ambiLightBlue[] = { 0.2f, 0.2f, 0.2f };
+    float specLightBlue[] = { 0.8f, 0.8f, 0.8f };
+
+    float diffViolet[] = { 0.5f, 0, 0.5f };
+    float ambiViolet[] = { 0.2f, 0.2f, 0.2f };
+    float specViolet[] = { 0.8f, 0.8f, 0.8f };
+
+    void drawString(void* font, const char* text, float x, float y)
+    {
+        if (!text)
+        {
+            return;
+        }
+        glRasterPos2f(x, y);
+        while (*text)
+        {
+            glutBitmapCharacter(font, *text);
+            text++;
+        }
+    }
+}
